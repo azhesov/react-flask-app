b0b73d5bec0db79be66b5cdb3036c70fc8c1d7a2
azhesov <75306255+anishewIQ@users.noreply.github.com>
2023-05-18 13:28:27 +0300

    added comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// React фронтальная часть приложения работает только с darts-mysql-flask-small задней частью приложения
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Определение устройства приложения
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
